6400ef95e927761745d25e06c1679886afe726f5
theswaginatorone <rob.tornblom@gmail.com>
2020-12-28 10:00:10 +0100

    Lagt till logout funktion i homeactivity

--- app/src/main/java/com/example/Tajming/HomeActivity.java
+++ app/src/main/java/com/example/Tajming/HomeActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity
 {
@@ -12,6 +15,7 @@ public class HomeActivity extends AppCompatActivity
     private Button button_time_home;
     private Button button_profile_home;
     private Button button_mileage_home;
+    private Button button_logout_home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -22,6 +26,7 @@ public class HomeActivity extends AppCompatActivity
         button_time_home = findViewById(R.id.menu_time);
         button_profile_home = findViewById(R.id.menu_profile);
         button_mileage_home = findViewById(R.id.menu_mileage);
+        button_logout_home = findViewById(R.id.button_logout_home);
 
         button_time_home.setOnClickListener(v ->
         {
@@ -39,6 +44,18 @@ public class HomeActivity extends AppCompatActivity
         {
             Intent intent = new Intent(HomeActivity.this, MileageActivity.class);
             startActivity(intent);
+        });
+
+        button_logout_home.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
         });
     }
 }
