352d7b766adfec1db322f6fb5f41dd9c67308df6
Robin Törnblom <rob.tornblom@gmail.com>
2021-01-11 17:30:31 +0100

    Fixade reporttime med timepickerspinners

--- app/src/main/java/com/example/Tajming/ReportTimeActivity.java
+++ app/src/main/java/com/example/Tajming/ReportTimeActivity.java
@@ -14,11 +14,13 @@ public class ReportTimeActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_time);
-        Intent intent = getIntent();
+       /* Intent intent = getIntent();
         int year = intent.getIntExtra("year", 0);
         int month = intent.getIntExtra("month", 0);
         int dayOfMonth = intent.getIntExtra("dayOfMonth", 0);
         TextView pickedDate = (TextView) findViewById(R.id.textField_picked_date);
         pickedDate.setText(year + ":" + month +":" + dayOfMonth);
+
+        */
     }
 }
