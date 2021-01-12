8b27ffd98e97dfae0a6cd4ee110de1f8b175b547
18ropy01 <ronjapyykko@hotmail.com>
2021-01-12 23:12:02 +0100

    Finat till i koden

--- app/src/main/java/com/example/Tajming/java/MilageManager.java
+++ app/src/main/java/com/example/Tajming/java/MilageManager.java
@@ -1,28 +1,19 @@
 package com.example.Tajming.java;
 
 import android.os.Build;
-import android.widget.ArrayAdapter;
-
 import androidx.annotation.RequiresApi;
 
 import java.text.SimpleDateFormat;
-import java.time.Month;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
 public class MilageManager {
 
     private List<Milage> milageList;
-    private String totalTimeMonth;
-    private String stringHour;
-    private String stringMin;
-    private String stringSec;
     private int totalKilometer;
     Double totalMile;
-
 
     public MilageManager(){
         milageList = new ArrayList<Milage>();
@@ -38,58 +29,39 @@ public class MilageManager {
 
     public double getTotalMilageMonth(String month){
         totalKilometer = 0;
+        String monthInDate = null;
+        if (month == "jan"){
+            monthInDate = "Jan";
+        }else if(month == "feb"){
+            monthInDate = "Feb";
+        }else if(month == "mar"){
+            monthInDate = "Mar";
+        }else if(month == "apr"){
+            monthInDate = "Apr";
+        }else if(month == "may"){
+            monthInDate = "May";
+        }else if(month == "jun"){
+            monthInDate = "Jun";
+        }else if(month == "jul"){
+            monthInDate = "Jul";
+        }else if(month == "aug"){
+            monthInDate = "Aug";
+        }else if(month == "sep"){
+            monthInDate = "Sep";
+        }else if(month == "oct"){
+            monthInDate = "Oct";
+        }else if(month == "nov"){
+            monthInDate = "Nov";
+        }else if(month == "dec"){
+            monthInDate = "Dec";
+        }
         for(Milage milage : milageList){
-            if (month == "jan"){
-                if(milage.getDate().substring(8) == "Jan"){
-                    totalKilometer = totalKilometer + milage.getKilometer();
-                }
-            }else if(month == "feb"){
-                if(milage.getDate().substring(8) == "Feb"){
-                    totalKilometer = totalKilometer + milage.getKilometer();
-                }
-            }else if(month == "mar"){
-                if(milage.getDate().substring(8) == "Mar"){
-                    totalKilometer = totalKilometer + milage.getKilometer();
-                }
-            }else if(month == "apr"){
-                if(milage.getDate().substring(8) == "Apr"){
-                    totalKilometer = totalKilometer + milage.getKilometer();
-                }
-            }else if(month == "may"){
-                if(milage.getDate().substring(8) == "May"){
-                    totalKilometer = totalKilometer + milage.getKilometer();
-                }
-            }else if(month == "jun"){
-                if(milage.getDate().substring(8) == "Jun"){
-                    totalKilometer = totalKilometer + milage.getKilometer();
-                }
-            }else if(month == "jul"){
-                if(milage.getDate().substring(8) == "Jul"){
-                    totalKilometer = totalKilometer + milage.getKilometer();
-                }
-            }else if(month == "aug"){
-                if(milage.getDate().substring(8) == "Aug"){
-                    totalKilometer = totalKilometer + milage.getKilometer();
-                }
-            }else if(month == "sep"){
-                if(milage.getDate().substring(8) == "Sep"){
-                    totalKilometer = totalKilometer + milage.getKilometer();
-                }
-            }else if(month == "oct"){
-                if(milage.getDate().substring(8) == "Oct"){
-                    totalKilometer = totalKilometer + milage.getKilometer();
-                }
-            }else if(month == "nov"){
-                if(milage.getDate().substring(8) == "Nov"){
-                    totalKilometer = totalKilometer + milage.getKilometer();
-                }
-            }else if(month == "dec"){
-                if(milage.getDate().substring(8) == "Dec"){
-                    totalKilometer = totalKilometer + milage.getKilometer();
-                }
+            if(milage.getDate().substring(8) == monthInDate){
+                totalKilometer = totalKilometer + milage.getKilometer();
             }
         }
-        return (double) totalKilometer/10;
+        totalMile = (double) totalKilometer/10;
+        return totalMile;
     }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
