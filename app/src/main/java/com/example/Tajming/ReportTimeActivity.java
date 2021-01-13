28a06649e14797d8f8233745616b3f2d8035fd9d
Robin Törnblom <rob.tornblom@gmail.com>
2021-01-13 11:59:20 +0100

    grundkoden för ändring av tiden i spinners

--- app/src/main/java/com/example/Tajming/ReportTimeActivity.java
+++ app/src/main/java/com/example/Tajming/ReportTimeActivity.java
@@ -2,12 +2,19 @@ package com.example.Tajming;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
 
 public class ReportTimeActivity extends AppCompatActivity
 {
+    private TimePicker timePicker_start_work;
+    private TimePicker timePicker_stop_work;
+    private TimePicker timePicker_start_break;
+    private TimePicker timePicker_stop_break;
+    private Button button_save_time_changes;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -22,5 +29,57 @@ public class ReportTimeActivity extends AppCompatActivity
         pickedDate.setText(year + ":" + month +":" + dayOfMonth);
 
         */
+
+        timePicker_start_work = findViewById(R.id.timePicker_start_working);
+        timePicker_stop_work = findViewById(R.id.timePicker_stop_working);
+        timePicker_start_break = findViewById(R.id.timePicker_start_break);
+        timePicker_stop_break = findViewById(R.id.timePicker_stop_break);
+        button_save_time_changes = findViewById(R.id.button_edit_day_save);
+
+
+        timePicker_start_work.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener()
+        {
+            @Override
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute)
+            {
+                date = String.valueOf(hourOfDay).toString() + ":" + String.valueOf(minute).toString();
+            }
+        });
+
+        timePicker_stop_work.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener()
+        {
+            @Override
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute)
+            {
+                date = String.valueOf(hourOfDay).toString() + ":" + String.valueOf(minute).toString();
+            }
+        });
+
+        timePicker_start_break.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener()
+        {
+            @Override
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute)
+            {
+                date = String.valueOf(hourOfDay).toString() + ":" + String.valueOf(minute).toString();
+            }
+        });
+
+        timePicker_stop_break.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener()
+        {
+            @Override
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute)
+            {
+                date = String.valueOf(hourOfDay).toString() + ":" + String.valueOf(minute).toString();
+            }
+        });
+
+        button_save_time_changes.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+            }
+        });
     }
 }
